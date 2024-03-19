de8b0a27d8a80db2a3a9b2693df1e43ae543cd22
ek <ek@biscuits>
2024-03-18 23:52:11 -0400

    Getting data and making soup

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+# imports
 import requests
 from bs4 import BeautifulSoup
 
-# basic request
-# response = requests.get("https://oxylabs.io/")
-# print(response.text)
-
-# with form
-# form_data = {'key1': 'value1', 'key2': 'value2'}
-# response = requests.post('https://httpbin.org/post', data=form_data)
-# print(response.text)
-
-# getting pokemon
-url = "https://pokemon.fandom.com/wiki/List_of_Pok%C3%A9mon"
+# url endpoint
+url = 'https://pokemon.fandom.com/wiki/List_of_Pok%C3%A9mon'
+# respose from request 
 response = requests.get(url)
+# making soup with response text
 soup = BeautifulSoup(response.text, 'html.parser') 
-print(soup.title)
+# find first table using attribute and class name
+table = soup.find('table', class_='wikitable')
+# initialize empty header and row tuple
+header = []
+rows = []
+
+# loop through table rows
+for i, row in enumerate(table.find_all('tr')):
+    # if first row, set header
+    if i == 0:
+        header = [el.text.strip() for el in row.find_all('th')]
+    # otherwise append each row to the rows tuple
+    else:
+        rows.append([el.text.strip() for el in row.find_all('td')])
+
+for row in rows:
+    print(row)
+
+
+# ['#', 'Sprite', 'Name', 'Type 1', 'Type 2', 'Japanese', 'Official rom.']
+print(header)
